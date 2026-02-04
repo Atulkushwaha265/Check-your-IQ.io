a52fc054a21265d238a5b76be48e7d10bb56b6ea
Atulkushwaha265 <atulk86011@gmail.com>
2026-02-05 00:31:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,14 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def create_folders():
-    base_path = "/storage/emulated/0/MyFolders"
+    base_path = "data/MyFolders"
 
     os.makedirs(base_path, exist_ok=True)
 
-    for i in range(1, 1000):
+    for i in range(1, 1000000000):
         os.makedirs(os.path.join(base_path, f"Project{i}"), exist_ok=True)
 
     return "Folders created successfully 👍"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
+
